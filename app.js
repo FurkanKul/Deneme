b41ff0cc6a6cecbd04ecf804281d3dc0694dfe97
Furkan Kul <furkankuul@gmail.com>
2024-10-07 13:55:09 +0300

    join-create buttons added

--- app.js
+++ app.js
@@ -8,3 +8,22 @@ function toggleMenu() {
       sideMenu.classList.add("closed");
   }
 }
+// Pop-up açma fonksiyonu
+function openPopup(buttonType) {
+  var popup = document.getElementById('popup');
+  var popupText = document.getElementById('popup-text');
+
+  if (buttonType === 'katil') {
+      popupText.innerHTML = 'Katılma Ekranı';
+  } else if (buttonType === 'olustur') {
+      popupText.innerHTML = 'Oluşturma Ekranı';
+  }
+
+  popup.style.display = 'flex';
+}
+
+// Pop-up kapatma fonksiyonu
+function closePopup() {
+  var popup = document.getElementById('popup');
+  popup.style.display = 'none';
+} 
